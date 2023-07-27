8b2e7ed97802435edbf7b6e2396ed7468f09154c
Juan Lozada <lozadagomezj@gmail,com>
2023-07-27 15:11:49 -0500

    modulo empleado

--- Empleado.cs
+++ Empleado.cs
@@ -1,19 +1,22 @@
 // Id (entero), Nombre (cadena de caracteres), Edad (entero), Salario (decimal) y Departamento (cadena de caracteres).
 
-public class Empleado 
+namespace gestión_empleados
 {
-    public long Id { get; set;}
-    public string? Nombre { get; set;}
-    public int? Edad { get; set;}
-    public float? Salario { get; set;}
-    public string? Departamento { get; set;}
-
-    public Empleado(long id, string? nombre, int? edad, float? salario, string? departamento)
+    public class Empleado
     {
-        Id = id;
-        Nombre = nombre;
-        Edad = edad;
-        Salario = salario;
-        Departamento = departamento;
+        public long Id { get; set; }
+        public string? Nombre { get; set; }
+        public int? Edad { get; set; }
+        public float? Salario { get; set; }
+        public string? Departamento { get; set; }
+
+        public Empleado(long id, string nombre, int edad, float salario, string departamento)
+        {
+            Id = id;
+            Nombre = nombre;
+            Edad = edad;
+            Salario = salario;
+            Departamento = departamento;
+        }
     }
-} 
+}
--- Program.cs
+++ Program.cs
@@ -3,68 +3,80 @@
 using System;
 using System.Collections.Generic;
 
-class Program 
+
+namespace gestión_empleados
 {
-    static Dictionary<long, Empleado> empleados = new();
 
-    static void Main() {
-        int opcion;
+    class Program 
+    {
+        static Dictionary<long, Empleado> empleados = new();
 
-        do 
-        {
-            Menu();
-            Console.Write("Elige una opción: ");
-            opcion = Convert.ToInt32(Console.ReadLine());
+        static void Main() {
+            int opcion;
 
-            switch (opcion)
+            do 
             {
-                case 1: 
-                    Agregar();
-                    break;
-            }
-        } while (opcion != 7);
-    }
+                Menu();
+                Console.Write("Elige una opción: ");
+                opcion = Convert.ToInt32(Console.ReadLine());
 
-    static void Menu() {
-        Console.Clear();
-        Console.WriteLine("Menú de opciones:");
-        Console.WriteLine("1. Agregar empleado");
-        Console.WriteLine("2. Mostrar un empleado");
-        Console.WriteLine("3. Listar los empleados");
-        Console.WriteLine("4. Salario promedio de la empresa");
-        Console.WriteLine("5. Salario promedio de un departamento");
-        Console.WriteLine("6. Buscar empleados");
-        Console.WriteLine("7. Salir");
-    }
-
-    static void Agregar() {
-
-        Console.WriteLine("Ingresa el nombre:");
-        string? nombre = Console.ReadLine();
-
-        Console.WriteLine("Ingresa el numero de identificacion");
-        if (long.TryParse(Console.ReadLine(), out long id)) 
-        {    
-            Console.WriteLine("Ingresa la edad:");
-            if (int.TryParse(Console.ReadLine(), out int edad)) 
-            {
-                Console.WriteLine("Ingresa el salario:");
-                if (float.TryParse(Console.ReadLine(), out float salario))
+                switch (opcion)
                 {
-                    Console.WriteLine("Ingresa el departamento");
-                    string? departamento = Console.ReadLine();
+                    case 1: 
+                        Agregar();
+                        break;
+                    case 2:
+                        MostrarEmpleado();
+                        break;
+                }
+            } while (opcion != 7);
+        }
 
-                    // Crear nuevo empleado
-                    var empleado = new Empleado(nombre: nombre, edad: edad, salario: salario, departamento: departamento, id: id);
+        static void Menu() {
+            Console.WriteLine("Menú de opciones:");
+            Console.WriteLine("1. Agregar empleado");
+            Console.WriteLine("2. Mostrar un empleado");
+            Console.WriteLine("3. Listar los empleados");
+            Console.WriteLine("4. Salario promedio de la empresa");
+            Console.WriteLine("5. Salario promedio de un departamento");
+            Console.WriteLine("6. Buscar empleados");
+            Console.WriteLine("7. Salir");
+        }
 
-                    // Añadir al empleado en diccionario de empleados
-                    empleados.Add(key: id, value: empleado);
+        static void Agregar() {
 
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("¡Empleado agregado con éxito!");
-                    Console.ResetColor();
-                } 
+            Console.WriteLine("Ingresa el nombre:");
+            string? nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingresa el numero de identificacion");
+            if (long.TryParse(Console.ReadLine(), out long id)) 
+            {    
+                Console.WriteLine("Ingresa la edad:");
+                if (int.TryParse(Console.ReadLine(), out int edad)) 
+                {
+                    Console.WriteLine("Ingresa el salario:");
+                    if (float.TryParse(Console.ReadLine(), out float salario))
+                    {
+                        Console.WriteLine("Ingresa el departamento");
+                        string? departamento = Console.ReadLine();
+
+                        // Crear nuevo empleado
+                        var empleado = new Empleado(nombre: nombre ?? "NN", edad: edad, salario: salario, departamento: departamento ?? "NN", id: id);
+
+                        // Añadir al empleado en diccionario de empleados
+                        empleados.Add(key: id, value: empleado);
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("¡Empleado agregado con éxito!");
+                        Console.ResetColor();
+                    } 
+                }
             }
         }
+
+        static void MostrarEmpleado() {
+
+        }
+
     }
 }
